5c25f8a20ffd2480db7e04f6b8647e9d5d81b1ee
aashish <aashishkrd1@gmail.com>
2026-03-10 13:34:59 +0530

    links fa import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaMedium, FaWordpress, FaInstagram } from "react-icons/fa";
 import { useState } from "react";
 import { AnimatePresence } from "framer-motion";
 import HeroLogo from "./components/HeroLogo.jsx";
